95ea288e24eac02f541b7d94d8a096e86ef450e7
Ray0v0 <947609598@qq.com>
2023-02-28 17:16:17 +0800

    Add some note. Add a new interface to judge whether x and y come from a same tree. Delete the rank of leaf node.

--- src/dataStructure/UnionFindSet.java
+++ src/dataStructure/UnionFindSet.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 
 public class UnionFindSet {
     int size = 0;
-    ArrayList<Integer> par = new ArrayList<>();
-    HashMap<Integer, Integer> rank = new HashMap<>();
+    ArrayList<Integer> par = new ArrayList<>(); // store parent
+    HashMap<Integer, Integer> rank = new HashMap<>(); // store the rank of a tree
+
+    // initializing the UnionFindSet with n individuals
     public void init(int n) {
         this.size = n;
         for (int i = 0; i < n; i++){
@@ -14,6 +16,8 @@ public class UnionFindSet {
             rank.put(i, i);
         }
     }
+
+    // find x's ancestor
     public int find(int x) {
         if (par.get(x) == x) {
             return x;
@@ -22,16 +26,21 @@ public class UnionFindSet {
             return par.get(x);
         }
     }
+
+    // unite two trees x and y
     public void unite(int x, int y) {
         x = find(x);
         y = find(y);
         if (x == y) {
             return ;
         }
+        // always unite the small one under the big one to prevent degeneration
         if (rank.get(x) < rank.get(y)) {
             par.set(x, y);
+            rank.remove(x);
         } else {
             par.set(y, x);
+            rank.remove(y);
             if (rank.get(x).equals(rank.get(y))) {
                 int rankTemp = rank.get(x);
                 rank.remove(x);
@@ -39,6 +48,8 @@ public class UnionFindSet {
             }
         }
     }
+
+    // print the relationship
     public void print() {
         System.out.println("This is the begin of print.");
         for (int i = 0; i < size; i++) {
@@ -46,6 +57,13 @@ public class UnionFindSet {
         }
         System.out.println("This is the end of print.");
     }
+
+    // judge whether x and y come from the same tree
+    public boolean isSame(int x, int y) {
+        return find(x) == find(y);
+    }
+
+    
     public static void main(String[] args) {
         UnionFindSet s = new UnionFindSet();
         s.init(5);
